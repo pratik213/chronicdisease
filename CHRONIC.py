2007324c959c4f05385f9db80963a9159752d2f8
pratik213 <51071026+pratik213@users.noreply.github.com>
2020-09-09 15:41:37 +0545

    Update CHRONIC.py

--- CHRONIC.py
+++ CHRONIC.py
@@ -7,12 +7,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder,MinMaxScaler
 import matplotlib.pyplot as plt
 import keras as k
-df=pd.read_csv("chronicdisease.csv")
+df=pd.read_csv("kidney_disease.csv")
 columns_to_retain=['sg','al','sc','hemo','pcv','wbcc','rbcc','htn','classification']
-df=df.drop(['id',"'age'", "'bp'","'su'", "'rbc'", "'pc'", "'pcc'",
-       "'ba'", "'bgr'", "'bu'","'sod'", "'pot'",
-       "'dm'", "'cad'", "'appet'",
-       "'pe'", "'ane'", 'Unnamed: 26'],axis=1)
+df=df.drop(['id',"age", "bp","su", "rbc", "pc", "pcc",
+       "ba", "bgr", "bu","sod", "pot",
+       "dm", "cad", "appet",
+       "pe", "ane"],axis=1)
 df=df.dropna(axis=0)
 #Transform the non-numeric data in the columns
 for column in df.columns:
@@ -20,8 +20,8 @@ for column in df.columns:
         continue
     df[column]=LabelEncoder().fit_transform(df[column])
 #split the data into independent (x) dataset(the features) and dependent (y) dataset(the target)
-X=df.drop(["'class'"],axis=1)
-y=df["'class'"]
+X=df.drop(["classification"],axis=1)
+y=df["classification"]
 #Feature Scaling
 #min-max scaler method scales the data set so that all the input lie between 0 and 1
 x_scaler=MinMaxScaler()
@@ -37,7 +37,7 @@ model.add(Dense(1,activation='hard_sigmoid'))
 #Compile the model
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 #train the model
-history=model.fit(X_train,y_train,epochs=2000,batch_size=X_train.shape[0])
+history=model.fit(X_train,y_train,epochs=500,batch_size=X_train.shape[0])
 #save the model
 model.save('ckd.model')
 #visualize the model loss and accuracy
